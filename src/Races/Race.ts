8f698492eb568a8e8e247a6750c82c9864f711c0
Otávio Azevedo <otavioad1997@gmail.com>
2022-10-18 15:29:47 -0300

    Requisito 1 - Classe Race

---
+++ src/Races/Race.ts
@@ -0,0 +1,27 @@
+abstract class Race {
+  private _name: string;
+  private _dexterity: number;
+  private static _createdRacesInstances: number;
+  
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+    Race._createdRacesInstances += 1;
+  }
+
+  get name(): string {
+    return this._name;
+  } 
+
+  get dexterity(): number {
+    return this._dexterity;
+  } 
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+}
+
+export default Race;
